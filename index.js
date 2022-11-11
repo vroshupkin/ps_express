3b70a8d2ead75ff1569df0c68f72becd465d2928
vroshupkin <vroshupkin1@gmail.com>
2022-11-11 21:57:00 +0300

    Добавлены основные функции на express

--- index.js
+++ index.js
@@ -2,43 +2,51 @@ import { helperProgress } from './src/common/progress.js'
 import { ChildProcess } from 'child_process'
 import http from 'http'
 import { Minute } from './src/common/time.js'
-import { express } from 'express'
+import express from 'express'
+import { appendFile } from 'fs'
 
 const host = '127.0.0.1'
 const port = 8000
+const app = express()
+
+/* 
+    Порядок обработки запросов зависит от их регистрации в коде. При этом для запуска следующей цепочки
+    требуется вызывать next()
+*/ 
+
+/* 
+    Можно использовать регулярные выражения в роутингах
+ */
 
 
-
-const router = {
-    get: {
-        hello: (res) => {
-            res.statusCode = 200
-            res.setHeader('Content-type', 'text/plain')
-            res.end('Привет методы и энд поинты url!')
-        }
-    }
+const cb_1 = (req, res, next) => {
+    console.log('cb_1')
+    next()
+}
+const cb_2 = (req, res, next) => {
+    console.log('cb_2')
+    res.send('Hello middleware')
+    next()
 }
 
-const server = http.createServer((req, res) => {
-    const endPoint = req.url.slice(1)
-
-    const handler = router[req.method.toLowerCase()]?.[endPoint]
-    
-    if(handler)>>>
-        handler(res)
-    else{
-        res.statusCode = 200
-        res.setHeader('Content-type', 'text/plain')
-        res.end('Привет!')
-    }
-        
-
-    
+// Вызывается при любом методе
+app.all('/hello', (req, res, next) => {
+    console.log('all')
+    next()
 })
 
-server.listen(port, host, () => {
+app.get('/hello', cb_1, cb_2)
+
+app.post('/hello', (req, res) => {
+
+})
+
+
+
+app.listen(port, () => {
     console.log(`Сервер запущен ${host}:${port}`)
 })
+
 
 
 function printProgress(step_progress){
@@ -49,7 +57,7 @@ function printProgress(step_progress){
     console.log(`Ступень ${step_progress + 1}/${task_times.length} ` + helperProgress(task_times, step_progress))
 }
 
-printProgress(2)
+printProgress(3)
 
 // for (let i = 0; i <= task.length; i++) {    
 //     console.log(helperProgress(task, i))
