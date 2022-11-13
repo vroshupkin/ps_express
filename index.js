f8305b6924dd99847fe6a819bf210156266d9bcf
vroshupkin <vroshupkin1@gmail.com>
2022-11-13 13:57:57 +0300

    Переделан helper

--- index.js
+++ index.js
@@ -19,23 +19,11 @@ const app = express()
  */
 
 
-const cb_1 = (req, res, next) => {
-    console.log('cb_1')
-    next()
-}
-const cb_2 = (req, res, next) => {
-    console.log('cb_2')
-    res.send('Hello middleware')
-    next()
-}
-
 // Вызывается при любом методе
-app.all('/hello', (req, res, next) => {
+app.get('/hello', (req, res, next) => {
     console.log('all')
     next()
 })
-
-app.get('/hello', cb_1, cb_2)
 
 app.post('/hello', (req, res) => {
 
@@ -49,15 +37,68 @@ app.listen(port, () => {
 
 
 
-function printProgress(step_progress){
-    const task_times = [9.18, 4.20, 10.27, 9.36, 5.32, 9.15]
-    const names = ['Вводное видео', 'Простой http сервер', 'Переходим на express', 'Маршрутизация', 'Ответы клиенту', 'Router', 'Промежуточные обработчики']
-    
-    console.log(`${step_progress + 1} Ступень: ${names[step_progress]}`)
-    console.log(`Ступень ${step_progress + 1}/${task_times.length} ` + helperProgress(task_times, step_progress))
+
+const node_js_course = {
+    1: {
+        name: 'Почему NodeJS для backend?',
+        videos: {
+            1: {
+                name: 'Почему NodeJS для backend?',
+                time: 11.34
+            },
+        },
+    },
+
+    9: {
+        name: 'Приложение 2 - API с ExpressJS',
+        videos: {
+            1: {
+                name: 'Вводное видео',
+                time: 1.59              
+            },
+            2: {
+                name: 'Простой http сервер',
+                time: 9.18
+            },
+            3: {
+                name: 'Переходим на express',
+                time: 4.20
+            },
+            4: {
+                name: 'Марщрутизация',
+                time: 10.27
+            },
+            5: {
+                name: 'Ответы клиенту',
+                time: 9.36
+            },
+            6: {
+                name: 'Router',
+                time: 5.32
+            },
+            7: {
+                name: 'Промежуточные обработчики',
+                time: 9.15
+            },
+        }
+    }
 }
 
-printProgress(3)
+
+function printProgress(step, step_videos){
+    const minutes_arr = Object.values(node_js_course[step].videos).map(val => val.time)
+    const names_arr = Object.values(node_js_course[step].videos).map(val => val.name)
+
+    const step_name = node_js_course[step].name
+    console.log(`Ступень ${step}: '${step_name}'`)
+    console.log(
+        `Видео ` +  
+        `${step_videos}/${minutes_arr.length}: ` + `'${names_arr[step_videos - 1]}' ` +
+        `${helperProgress(minutes_arr, step_videos)}`
+    )
+}
+
+printProgress(9, 5)
 
 // for (let i = 0; i <= task.length; i++) {    
 //     console.log(helperProgress(task, i))
