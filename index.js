12f356d834c0290eaf1f669518f316702f7e77de
vroshupkin <vroshupkin1@gmail.com>
2022-11-13 14:11:03 +0300

    Рефакторинг helper progress

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import { helperProgress } from './src/common/progress.js'
+
 import { ChildProcess } from 'child_process'
 import http from 'http'
-import { Minute } from './src/common/time.js'
 import express from 'express'
 import { appendFile } from 'fs'
+import { printProgress } from './src/services/course_helper.js'
 
 const host = '127.0.0.1'
 const port = 8000
@@ -37,66 +37,6 @@ app.listen(port, () => {
 
 
 
-
-const node_js_course = {
-    1: {
-        name: 'Почему NodeJS для backend?',
-        videos: {
-            1: {
-                name: 'Почему NodeJS для backend?',
-                time: 11.34
-            },
-        },
-    },
-
-    9: {
-        name: 'Приложение 2 - API с ExpressJS',
-        videos: {
-            1: {
-                name: 'Вводное видео',
-                time: 1.59              
-            },
-            2: {
-                name: 'Простой http сервер',
-                time: 9.18
-            },
-            3: {
-                name: 'Переходим на express',
-                time: 4.20
-            },
-            4: {
-                name: 'Марщрутизация',
-                time: 10.27
-            },
-            5: {
-                name: 'Ответы клиенту',
-                time: 9.36
-            },
-            6: {
-                name: 'Router',
-                time: 5.32
-            },
-            7: {
-                name: 'Промежуточные обработчики',
-                time: 9.15
-            },
-        }
-    }
-}
-
-
-function printProgress(step, step_videos){
-    const minutes_arr = Object.values(node_js_course[step].videos).map(val => val.time)
-    const names_arr = Object.values(node_js_course[step].videos).map(val => val.name)
-
-    const step_name = node_js_course[step].name
-    console.log(`Ступень ${step}: '${step_name}'`)
-    console.log(
-        `Видео ` +  
-        `${step_videos}/${minutes_arr.length}: ` + `'${names_arr[step_videos - 1]}' ` +
-        `${helperProgress(minutes_arr, step_videos)}`
-    )
-}
 
 printProgress(9, 5)
 
