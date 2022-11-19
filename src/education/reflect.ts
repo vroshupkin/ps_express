eeb5ea5dd82c20ff1484924456ca7531bb2554f3
vroshupkin <vroshupkin1@gmail.com>
2022-11-19 03:46:15 +0300

    Добавлен набросок Inject и Injactable

---
+++ src/education/reflect.ts
@@ -0,0 +1,39 @@
+import 'reflect-metadata'
+
+// function Inject(key: string) {
+//     return (target: Function) => {
+//         Reflect.defineMetadata('a', 1, target)
+
+//         const meta = Reflect.getMetadata('a', target)
+//         console.log(meta)
+//     }
+// }
+
+function Injectable(key: string) {
+    return (target: Function) => {
+        Reflect.defineMetadata(key, 1, target)
+
+        const meta = Reflect.getMetadata(key, target)
+        console.log(meta)
+    }
+}
+
+function Prop(
+    target:{},
+    name: string
+    ) {
+    
+}
+
+@Injectable('C')
+export class C{
+    @Prop prop: number
+
+}
+
+@Injectable('D')
+export class D{
+    constructor(@Inject('C') c: C){
+
+    }
+}
